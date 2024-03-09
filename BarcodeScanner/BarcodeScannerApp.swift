55a8fef3ca601ba7b3b4c4938f84c40b35e04c0c
giovannimhern <giovannimhern@uchicago.edu>
2024-03-08 20:47:29 -0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Giovanni Maya on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
